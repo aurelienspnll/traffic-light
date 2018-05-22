7ed13b7c15e02de36d9719c6e9a2ff128b215a6d
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-05-22 16:11:21 +0200

    Test simple light traffic

---
+++ simple-traffic-light-test/LTL/run-tests-P1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-init-false.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-init-true.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-rand.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-cpt-decale.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-true.smv
---
+++ simple-traffic-light-test/LTL/run-tests-P2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P2/test-rand.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P2/test-car-false.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P2/test-car-true.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P2/test-ped-false.smv
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P2/test-ped-true.smv
