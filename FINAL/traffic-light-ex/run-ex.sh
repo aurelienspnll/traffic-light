0af0bd4e26a0e23d41c8b668fe9ca40c1272c87a
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-05-26 13:48:31 +0200

    FINAL

---
+++ FINAL/traffic-light-ex/run-ex.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV -int ./traffic-light-ex.smv
---
+++ FINAL/traffic-light-ex/tests/run-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./ExtTestP2Pieton.smv
+
