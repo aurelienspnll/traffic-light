42cf07adc5bf671e4f66211bb7d9264f0de1d824
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-05-26 14:01:43 +0200

    FINAL

--- FINAL/simple-traffic-light/tests/LTL/run-tests-P1.sh
+++ FINAL/simple-traffic-light/tests/LTL/run-tests-P1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-init-false.smv
-/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-init-true.smv
-/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-rand.smv
-/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-cpt-decale.smv
-/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./P1/test-true.smv
+NuSMV ./P1/test-init-false.smv
+NuSMV ./P1/test-init-true.smv
+NuSMV ./P1/test-rand.smv
+NuSMV ./P1/test-cpt-decale.smv
+NuSMV ./P1/test-true.smv
--- FINAL/traffic-light-ex/tests/run-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/Users/aurelien/Documents/Fac/M1/SYSTEME-EMBARQUE/NuSMV-2.6.0-Darwin/bin/NuSMV ./ExtTestP2Pieton.smv
-
